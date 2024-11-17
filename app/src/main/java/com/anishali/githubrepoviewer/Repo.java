c0349cdd69006a9361ee8fa0290b2fa84cbb6586
Anish Ali <anishaliofficial@gmail.com>
2024-11-17 16:12:17 +0545

    Initial commit for Github Repo Viewer

---
+++ app/src/main/java/com/anishali/githubrepoviewer/Repo.java
@@ -0,0 +1,17 @@
+package com.anishali.githubrepoviewer;
+
+public class Repo {
+    private String name;
+    private String description;
+
+    //Getters
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+}
